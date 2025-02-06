eb65daf638dea35bb55ef53ae6bc4747690de13d
Phillip <phillipfleischer2@gmail.com>
2025-02-06 21:01:52 +0100

    feat: add bot role assignment to auto role

--- cogs/auto_role.py
+++ cogs/auto_role.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 import discord
 
 from ezcord import Cog
-from constants import MEMBER_ROLE_ID
+from constants import MEMBER_ROLE_ID, BOT_ROLE_ID
 
 if TYPE_CHECKING:
     from bot import YoungDevsClubBot
@@ -16,10 +16,15 @@ class AutoRoleCog(Cog):
 
     @Cog.listener()
     async def on_member_join(self, member: discord.Member):
-        member_role = await member.guild._fetch_role(MEMBER_ROLE_ID)  # noqa
+        role_id = MEMBER_ROLE_ID if not member.bot else BOT_ROLE_ID
+        member_role = await self._get_role(member.guild, role_id)
 
         if member_role is not None:
             await member.add_roles(member_role)
+
+    @staticmethod
+    async def _get_role(guild: discord.Guild, role_id: int) -> discord.Role:
+        return await guild._fetch_role(role_id)  # noqa
 
 
 def setup(bot: YoungDevsClubBot):
